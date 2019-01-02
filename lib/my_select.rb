f24aaa155c663e17aee4b65a91d4ca05805670e6
michael hussain <smhussain_1@yahoo.com>
2019-01-02 19:57:20 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+require "pry"
 def my_select(collection)
- # your code here!
-end
+  array = []
+  counter = 0 
+ 
+  while counter < collection.length 
+    if yield(collection[counter])
+     array.push(collection[counter]) 
+    end
+    counter += 1 
+    #array.each_with_index { |item, counter| if item == true then return #"#{counter}" } 
+    end 
+  array 
+end 
+
