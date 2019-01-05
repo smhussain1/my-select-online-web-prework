a84d322982d7e330ce7ff9fd9097cc484626f6e4
michael hussain <smhussain_1@yahoo.com>
2019-01-05 23:11:56 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -13,4 +13,14 @@ def my_select(collection)
   array 
 end 
 
-
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
+end
